c28ba822bb164e4065786a20022c98462d137e6b
Jasen <60624556+jasenscode@users.noreply.github.com>
2021-12-06 20:49:27 +0000

    .

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -25,22 +25,3 @@ const Card = (prop) => {
 };
 
 export default Card;
-
-{
-  /* <div className="card">
-      <div className="card__img">
-        <img src="https://images.unsplash.com/photo-1567291612096-635b1896679a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt={imgText} />
-      </div>
-      <div className="card__detail">
-        <div className="card__header">
-          <h3 className="card__name">buzz</h3>
-          <h4 className="card__miscellaneous">
-            ABV:
-            <span className="misc-detail"> 5%</span>
-          </h4>
-        </div>
-        <p className="card__tagline">A Real Bitter Experience.</p>
-        <p className="card__description">A light, crisp and bitter IPA brewed with English and American hops. A small batch brewed only once</p>
-      </div>
-    </div> */
-}
